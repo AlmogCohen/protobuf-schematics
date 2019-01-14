b733dabdef7c736dee68b74b7c0e74d8a32b9ac5
almogcohen <almog1232@gmail.com>
2019-01-14 12:13:21 -0800

    Bump version: 0.1.0 → 0.2.0

--- protobuf_schematics/__init__.py
+++ protobuf_schematics/__init__.py
@@ -3,4 +3,4 @@
 """Top-level package for ProtoBuf Schematics."""
 
 __author__ = """Almog Cohen"""
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     packages=find_packages(include=['protobuf_schematics']),
     test_suite='tests',
     url='https://github.com/AlmogCohen/protobuf-schematics',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
