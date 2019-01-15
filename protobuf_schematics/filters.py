7d791f499a8056c2cb38e579933bab1526cc724b
almogcohen <almog1232@gmail.com>
2019-01-15 15:26:18 -0800

    improve field converter code

--- protobuf_schematics/filters.py
+++ protobuf_schematics/filters.py
@@ -1,8 +1,20 @@
 """Define Jinja2 filters used in the templates compilation."""
 from pyrobuf import parse_proto
+from schematics.types import StringType, IntType, ModelType
+
+from protobuf_schematics.types import BytesType, EnumType
 
 
 class FieldConverter(object):
+
+    PROTOBUF_FIELD_TO_SCHEMATICS_FIELD = {
+        'string': StringType,
+        'uint32': IntType,
+        'uint64': IntType,
+        'bytes': BytesType,
+        'message': ModelType,
+        'enum': EnumType,
+    }
 
     @classmethod
     def convert(cls, field):  # type: (parse_proto.Parser.Field) -> str
@@ -16,42 +28,34 @@ class FieldConverter(object):
         except KeyError:
             raise ValueError('Unsupported Pyrobuf field token type: {}'.format(field.token_type))
 
-    @staticmethod
-    def _regular_field_converter(field):  # type: (parse_proto.Parser.Field) -> str
-        """Convert regular field to it's Schematics representation."""
-        if field.type == 'string':
-            field_repr = 'StringType()'
-        elif field.type == 'uint32' or field.type == 'uint64':
-            field_repr = 'IntType()'
-        elif field.type == 'bytes':
-            field_repr = 'BytesType()'
-        elif field.type == 'message':
-            field_repr = 'ModelType({})'.format(field.message_name)
-        elif field.type == 'enum':
-            field_repr = 'EnumType({})'.format(field.enum_def.name)
-        else:
-            raise ValueError('UNKNOWN FIELD TYPE. Type: {}, Name: {}'.format(field.type, field.name))
+    @classmethod
+    def _get_pyrobuf_field_repr(cls, field):
+        """Return the Schematics string representation of a Pyrobuf field type."""
+        try:
+            schematics_type = cls.PROTOBUF_FIELD_TO_SCHEMATICS_FIELD[field.type]
+        except KeyError:
+            raise ValueError('Unsupported Pyrobuf field type: {}. Field name: {}'.format(field.type, field.name))
 
+        type_name = schematics_type.__name__
+        arg = ''
+        if schematics_type == ModelType:
+            arg = field.message_name
+        elif schematics_type == EnumType:
+            arg = field.enum_def.name
+        return '{}({})'.format(type_name, arg)
+
+    @classmethod
+    def _regular_field_converter(cls, field):  # type: (parse_proto.Parser.Field) -> str
+        """Convert regular field to it's Schematics representation."""
+        field_repr = cls._get_pyrobuf_field_repr(field)
         if field.modifier == 'repeated':
             field_repr = 'ListType({})'.format(field_repr)
         return '{} = {}'.format(field.name, field_repr)
 
-    @staticmethod
-    def _map_field_converter(field):  # type: (parse_proto.Parser.MapField) -> str
+    @classmethod
+    def _map_field_converter(cls, field):  # type: (parse_proto.Parser.MapField) -> str
         """Convert map field to it's Schematics representation."""
-        if field.type == 'message':
-            value = 'ModelType({})'.format(field.message_name)
-        elif field.type == 'enum':
-            value = 'EnumType({})'.format(field.enum_def.name)
-        elif field.type == 'string':
-            value = 'StringType()'
-        elif field.type == 'uint32' or field.type == 'uint64':
-            value = 'IntType()'
-        elif field.type == 'bytes':
-            value = 'BytesType()'
-        else:
-            raise ValueError('UNKNOWN VALUE TYPE. Type: {}, Name: {}'.format(field.type, field.name))
-
+        value = cls._get_pyrobuf_field_repr(field)
         if field.key_type == 'string':
             key = 'str'
         elif field.key_type == 'uint32' or field.key_type == 'uint64':
@@ -59,6 +63,8 @@ class FieldConverter(object):
         elif field.key_type == 'bytes':
             key = 'str'
         else:
-            raise ValueError('UNKNOWN KEY TYPE. Type: {}, Name: {}'.format(field.type, field.name))
+            raise ValueError(
+                'Unsupported type for map field key. Key type: {}, Field name: {}'.format(field.type, field.name)
+            )
 
         return '{} = DictType({}, {})'.format(field.name, value, key)
