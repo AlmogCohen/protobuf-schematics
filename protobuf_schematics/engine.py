fc2643c9e6d03aa1c8e1421c72b95f221695c5f4
almogcohen <almog1232@gmail.com>
2019-01-15 15:13:08 -0800

    improve field converter code

--- protobuf_schematics/engine.py
+++ protobuf_schematics/engine.py
@@ -2,7 +2,7 @@
 from jinja2 import Environment, PackageLoader
 from pyrobuf.parse_proto import Parser
 
-from protobuf_schematics.filters import field_converter
+from protobuf_schematics.filters import FieldConverter
 
 
 def parse_proto_file(file_path):
@@ -30,6 +30,6 @@ def compile_parser_result(parser_dict):
 
     """
     _env = Environment(loader=PackageLoader('protobuf_schematics', 'templates'))
-    _env.filters['schematics_field'] = field_converter
+    _env.filters['schematics_field'] = FieldConverter.convert
     schematics_template = _env.get_template('schematics_py.tmpl')
     return schematics_template.render(parser_dict=parser_dict)
--- protobuf_schematics/filters.py
+++ protobuf_schematics/filters.py
@@ -2,56 +2,63 @@
 from pyrobuf import parse_proto
 
 
-def field_converter(field):  # type: (parse_proto.Parser.Field) -> str
-    """Convert a field to it's Schematics representation."""
-    if field.token_type == 'MAP_FIELD':
-        return _map_field_converter(field)
-    else:
-        return _regular_field_converter(field)
+class FieldConverter(object):
 
+    @classmethod
+    def convert(cls, field):  # type: (parse_proto.Parser.Field) -> str
+        """Convert a field to it's Schematics representation."""
+        token_type_to_handler = {
+            'MAP_FIELD': cls._map_field_converter,
+            'FIELD': cls._regular_field_converter,
+        }
+        try:
+            return token_type_to_handler[field.token_type](field)
+        except KeyError:
+            raise ValueError('Unsupported Pyrobuf field token type: {}'.format(field.token_type))
 
-def _regular_field_converter(field):  # type: (parse_proto.Parser.Field) -> str
-    """Convert regular field to it's Schematics representation."""
-    if field.type == 'string':
-        field_repr = 'StringType()'
-    elif field.type == 'uint32' or field.type == 'uint64':
-        field_repr = 'IntType()'
-    elif field.type == 'bytes':
-        field_repr = 'BytesType()'
-    elif field.type == 'message':
-        field_repr = 'ModelType({})'.format(field.message_name)
-    elif field.type == 'enum':
-        field_repr = 'EnumType({})'.format(field.enum_def.name)
-    else:
-        raise ValueError('UNKNOWN FIELD TYPE. Type: {}, Name: {}'.format(field.type, field.name))
+    @staticmethod
+    def _regular_field_converter(field):  # type: (parse_proto.Parser.Field) -> str
+        """Convert regular field to it's Schematics representation."""
+        if field.type == 'string':
+            field_repr = 'StringType()'
+        elif field.type == 'uint32' or field.type == 'uint64':
+            field_repr = 'IntType()'
+        elif field.type == 'bytes':
+            field_repr = 'BytesType()'
+        elif field.type == 'message':
+            field_repr = 'ModelType({})'.format(field.message_name)
+        elif field.type == 'enum':
+            field_repr = 'EnumType({})'.format(field.enum_def.name)
+        else:
+            raise ValueError('UNKNOWN FIELD TYPE. Type: {}, Name: {}'.format(field.type, field.name))
 
-    if field.modifier == 'repeated':
-        field_repr = 'ListType({})'.format(field_repr)
-    return '{} = {}'.format(field.name, field_repr)
+        if field.modifier == 'repeated':
+            field_repr = 'ListType({})'.format(field_repr)
+        return '{} = {}'.format(field.name, field_repr)
 
+    @staticmethod
+    def _map_field_converter(field):  # type: (parse_proto.Parser.MapField) -> str
+        """Convert map field to it's Schematics representation."""
+        if field.type == 'message':
+            value = 'ModelType({})'.format(field.message_name)
+        elif field.type == 'enum':
+            value = 'EnumType({})'.format(field.enum_def.name)
+        elif field.type == 'string':
+            value = 'StringType()'
+        elif field.type == 'uint32' or field.type == 'uint64':
+            value = 'IntType()'
+        elif field.type == 'bytes':
+            value = 'BytesType()'
+        else:
+            raise ValueError('UNKNOWN VALUE TYPE. Type: {}, Name: {}'.format(field.type, field.name))
 
-def _map_field_converter(field):  # type: (parse_proto.Parser.MapField) -> str
-    """Convert map field to it's Schematics representation."""
-    if field.type == 'message':
-        value = 'ModelType({})'.format(field.message_name)
-    elif field.type == 'enum':
-        value = 'EnumType({})'.format(field.enum_def.name)
-    elif field.type == 'string':
-        value = 'StringType()'
-    elif field.type == 'uint32' or field.type == 'uint64':
-        value = 'IntType()'
-    elif field.type == 'bytes':
-        value = 'BytesType()'
-    else:
-        raise ValueError('UNKNOWN VALUE TYPE. Type: {}, Name: {}'.format(field.type, field.name))
+        if field.key_type == 'string':
+            key = 'str'
+        elif field.key_type == 'uint32' or field.key_type == 'uint64':
+            key = 'int'
+        elif field.key_type == 'bytes':
+            key = 'str'
+        else:
+            raise ValueError('UNKNOWN KEY TYPE. Type: {}, Name: {}'.format(field.type, field.name))
 
-    if field.key_type == 'string':
-        key = 'str'
-    elif field.key_type == 'uint32' or field.key_type == 'uint64':
-        key = 'int'
-    elif field.key_type == 'bytes':
-        key = 'str'
-    else:
-        raise ValueError('UNKNOWN KEY TYPE. Type: {}, Name: {}'.format(field.type, field.name))
-
-    return '{} = DictType({}, {})'.format(field.name, value, key)
+        return '{} = DictType({}, {})'.format(field.name, value, key)
