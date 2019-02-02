e8b27eb141e9ee684b2ec442a7972bc352f993f8
almogcohen <almog1232@gmail.com>
2019-02-01 18:21:58 -0800

    add support for `get_mock_object` for custom types.

--- protobuf_schematics/types.py
+++ protobuf_schematics/types.py
@@ -1,4 +1,6 @@
+import random
 from base64 import b64decode, b64encode
+from random import choice
 
 from schematics.types import StringType
 
@@ -9,15 +11,27 @@ class EnumType(StringType):
         super(EnumType, self).__init__(**kwargs)
 
     def to_native(self, value, context=None):
+        if value in self.enum_class:
+            return value
         return self.enum_class[value]
 
     def to_primitive(self, value, context=None):
         return value.name
 
+    def _mock(self, context=None):
+        return choice(list(self.enum_class))
+
 
 class BytesType(StringType):
     def to_native(self, value, context=None):
+        if isinstance(value, bytes):
+            return value
         return b64decode(value)
 
     def to_primitive(self, value, context=None):
         return b64encode(value).decode('ascii')
+
+    def _mock(self, context=None):
+        # nasty hack to get the should be size
+        size = len(super(BytesType, self)._mock(context))
+        return bytes(random.getrandbits(8) for _ in range(size))
